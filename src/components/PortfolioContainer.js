e925033f28b0ecda6d5d3a4cb6f6c1f8b8d8c1e4
BenjiCCB <bensdecker+CCB@gmail.com>
2023-04-07 19:01:53 -0400

    jsx issues

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
+import '../styles/main.css';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -25,9 +26,19 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <h1>i'm a header</h1>
-    
     <div>
+      <header class="header-main">
+        <h1>Benji Decker</h1>
+        <p>Entrepreneur. Investor. General trouble maker || CEO, Bridge-X & Principal @ Bridge Capital</p>
+      </header>
+
+      <nav>
+        <a href="#about-me">About Me</a>
+        <a href="#roles-intro">Roles</a>
+        <a href="#contact-form">Contact</a>
+      </nav>
+  
+
       {/* // TODO: Add a comment describing what we are passing as props */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* // TODO: Add a comment explaining what is happening on the following line */}
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    // <!-- About me -->
+    <div class="about-me" id="about-me">
+      <h2>About me</h2>
+      {/* <!-- Profile pic --> */}
+      <div class="about-me-bio"><img src="./assets/images/profile.jpg" alt="profile image" />
+         <p>Entrepreneur.  Investor.  General trouble maker.<br><br>
+
+          Most recently, Principal at Bridge Capital, empowering champion businesses of tomorrow through funding and support.  Formerly EIR @Mercer, driving cross-organizational change and innovation for the world’s largest corporations.  Before this, I served as Global Community Director at WeWork, Global Director of WeWork Labs,  founder of the location based social network Kami, and lead on corporate strategy digital initiatives at NBCU.<br><br>
+        
+          I studied new media at Harvard for my undergrad, received a Masters at NYU in open innovation, and am a graduate of General Assembly's web development immersive program.<br><br>
+        
+          I believe in openness, honesty, and passion.  I'm an all around dork for all things digital and community building<br><br>
+         
+          Benji can be reached at:
+          <i class="fa-brands fa-linkedin"></i><a href="https://www.linkedin.com/in/benjidecker/">LinkedIn</a>
+          <i class="fa-brands fa-github"></i><a href="https://github.com/BenjiCCB">Github</a>
+          <i class="fa-brands fa-square-instagram"></i><a href="https://www.instagram.com/benji_decker/">Instagram</a>
+        </p>
     </div>
   );
 }
